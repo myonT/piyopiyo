b0e331c0f7137c8f47061fecb62e0d512e2f24f2
みょんた <murayamaayaka@MMMacBook-Pro.local>
2017-09-16 23:53:03 +0900

    init commit

---
+++ Assets/Script/PlayerScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerScript : MonoBehaviour {
+
+    public int score = 0;
+    public Text scoretext;
+
+	// Use this for initialization
+	void Start () {
+        scoretext.text = "Score : 0";
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            GetComponent<Rigidbody>().velocity = new Vector3(0,6,0);
+            GetComponent<AudioSource>().Play();
+        }
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+	{
+  //      if (collision.CompareTag("Player"))
+		//{
+		Debug.Log("GameOver");
+        SceneManager.LoadScene("GameOver");
+		//}
+	}
+
+    private void OnTriggerExit(Collider other)
+    {
+        score++;
+        Debug.Log(score);
+        scoretext.text = "Score : " + score;
+    }
+}
---
+++ Assets/Script/WallCriateScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallCriateScript : MonoBehaviour {
+    public GameObject wall;
+    float timer = 0f;
+    int interval = 3;
+
+	// Use this for initialization
+	void Start () {
+		Instantiate(wall, transform.position, transform.rotation);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if(timer >= interval){
+            transform.position = new Vector3(0, Random.Range(-2.5f, 2.5f), 6);//readonly
+            Instantiate(wall,transform.position,transform.rotation);
+            timer = 0;
+        }
+		
+	}
+}
